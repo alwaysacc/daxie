2d14e73d14f2b0a865b5230bc9556ece3ba453b6
alwaysacc <alwaysacc@163.com>
2019-01-09 16:43:21 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <el-row >
 
-    <el-col>
+      <el-col>
         <el-menu
           :default-active="activeIndex2"
           class="el-menu-demo"
@@ -17,27 +17,27 @@
 
           <el-row type="flex" align="middle" class="login">
             <el-col>
-            <el-autocomplete class="scarh"
-                             v-model="state4"
-                             :fetch-suggestions="querySearchAsync"
-                             placeholder="搜索" suffix-icon="el-icon-search"
-                             @select="handleSelect"
-            ></el-autocomplete>
+              <el-autocomplete class="scarh"
+                               v-model="state4"
+                               :fetch-suggestions="querySearchAsync"
+                               placeholder="搜索" suffix-icon="el-icon-search"
+                               @select="handleSelect"
+              ></el-autocomplete>
             </el-col>
             <el-col  :span="2">
               <el-menu-item index="4" class="login"><img src="./image/notice.png"/></el-menu-item></el-col>
             <el-col  :span="2">
-            <el-menu-item index="4" class="login">登录</el-menu-item></el-col>
+              <el-menu-item index="4" class="login">登录</el-menu-item></el-col>
             <el-col :span="2">
-            <el-menu-item index="5" class="login">注册</el-menu-item>
+              <el-menu-item index="5" class="login">注册</el-menu-item>
             </el-col>
           </el-row>
         </el-menu>
-    </el-col>
+      </el-col>
     </el-row>
     <div class="centers">
 
-<div class="left">
+      <div class="left">
         <el-menu
           default-active="2"
           class="el-menu-vertical-demo"
@@ -55,92 +55,90 @@
             <span slot="title">MySQL</span>
           </el-menu-item>
           <el-menu-item index="4" class="item">
-          <span slot="title">前端</span>
-        </el-menu-item>
+            <span slot="title">前端</span>
+          </el-menu-item>
           <el-menu-item index="4" >
             <span slot="title">前端</span>
           </el-menu-item>
         </el-menu>
-</div>
+      </div>
 
-<div class="center-div">
-  <h3>2018年总结以及2019年计划</h3>
-  <el-row>
-    <el-col  :span="24">
-      <span :class="{'con-art':seeall}">{{content}}</span>
-    </el-col>
+      <div class="center-div">
+        <h3>2018年总结以及2019年计划</h3>
+        <el-row>
+          <el-col  :span="24">
+            <span :class="{'con-art':seeall}">{{content}}</span>
+          </el-col>
 
-    <div @click='seeAll' style="float: right"><a >{{seeall?'查看全文':'收起'}}</a></div>
-  </el-row>
-  <el-row>
-    <el-col :span="6"><a href="#">点赞</a></el-col>
-    <el-col :span="6" ><a v-if="pl" @click="pinglun">55评论</a> <a v-if="!pl" @click="pinglun">收起评论</a></el-col>
-    <el-col :span="6"><a href="#">分享</a> </el-col>
-    <el-col :span="6"><a href="#">收藏</a> </el-col>
-  </el-row>
+          <div @click='seeAll' style="float: right"><a >{{seeall?'查看全文':'收起'}}</a></div>
+        </el-row>
+        <el-row>
+          <el-col :span="6"><a href="#">点赞</a></el-col>
+          <el-col :span="6" ><a v-if="pl" @click="pinglun">55评论</a> <a v-if="!pl" @click="pinglun">收起评论</a></el-col>
+          <el-col :span="6"><a href="#">分享</a> </el-col>
+          <el-col :span="6"><a href="#">收藏</a> </el-col>
+        </el-row>
 
-  <div v-if="show" style="border: #EBEBEB solid 1px; margin-top: 20px; padding-bottom: 10px;">
-    <el-row style="height:40px;line-height: 40px;background-color: #EBEBEB">
-      <el-col :span="4"style="font-weight:bold;">55条评论</el-col>
-      <el-col :span="4" style="float: right">切换排序</el-col>
-    </el-row>
-    <div style="border-bottom: #EBEBEB solid 1px; padding: 10px 0">
-      <el-row style="height:40px;line-height: 40px">
-        <el-col :span="4"><img src="./image/img.jpg"></img>hhhh</el-col>
-        <el-col :span="4" style="float: right">刚刚</el-col>
-      </el-row>
-      <el-row style="height:20px;line-height: 20px">
-        <el-col :span="4" style="float:left;padding-left: 50px">牛逼牛逼 </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="3" style="float:left;padding-left: 50px"><a>点赞</a></el-col>
-      </el-row>
-    </div>
-    <div style="border-bottom: #EBEBEB solid 1px; padding: 10px 0">
-      <el-row style="height:40px;line-height: 40px">
-        <el-col :span="4"><img src="./image/img.jpg"></img>hhhh</el-col>
-        <el-col :span="4" style="float: right">刚刚</el-col>
-      </el-row>
-      <el-row style="height:20px;line-height: 20px">
-        <el-col :span="4" style="float:left;padding-left: 50px">牛逼牛逼 </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="3" style="float:left;padding-left: 50px"><a>点赞</a></el-col>
-      </el-row>
-    </div>
-    <div style="margin-top: 10px;width: 100%">
-      <el-row>
-        <el-col :span="20">  <el-input v-model="input" placeholder="请输入内容" @focus="fsbutton" ></el-input></el-col>
-        <el-col :span="4">  <el-button type="primary" style="" v-if="button">主要按钮</el-button></el-col>
-      </el-row>
+        <div v-if="show" style="border: #EBEBEB solid 1px; margin-top: 20px; padding-bottom: 10px;">
+          <el-row style="height:40px;line-height: 40px;background-color: #EBEBEB">
+            <el-col :span="4"style="font-weight:bold;">55条评论</el-col>
+            <el-col :span="4" style="float: right">切换排序</el-col>
+          </el-row>
+          <div style="border-bottom: #EBEBEB solid 1px; padding: 10px 0">
+            <el-row style="height:40px;line-height: 40px">
+              <el-col :span="4"><img src="./image/img.jpg"></img>hhhh</el-col>
+              <el-col :span="4" style="float: right">刚刚</el-col>
+            </el-row>
+            <el-row style="height:20px;line-height: 20px">
+              <el-col :span="4" style="float:left;padding-left: 50px">牛逼牛逼 </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="3" style="float:left;padding-left: 50px"><a>点赞</a></el-col>
+            </el-row>
+          </div>
+          <div style="border-bottom: #EBEBEB solid 1px; padding: 10px 0">
+            <el-row style="height:40px;line-height: 40px">
+              <el-col :span="4"><img src="./image/img.jpg"></img>hhhh</el-col>
+              <el-col :span="4" style="float: right">刚刚</el-col>
+            </el-row>
+            <el-row style="height:20px;line-height: 20px">
+              <el-col :span="4" style="float:left;padding-left: 50px">牛逼牛逼 </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="3" style="float:left;padding-left: 50px"><a>点赞</a></el-col>
+            </el-row>
+          </div>
+          <div style="margin-top: 10px;width: 100%">
+            <el-row>
+              <el-col :span="24">
+                <el-input  class="el_input1" @blur="blur_input" v-bind:class="{el_input:isButton=='1'}" v-model="input" placeholder="请输入内容" @focus="fsbutton" ></el-input>
+              </el-col>
+            </el-row>
+          </div>
+        </div>
+      </div>
 
+      <div class="right">
+        <div class="right-top">
+          <el-row>
+            <el-col :span="8"><i class="el-icon-circle-plus-outline"/><br/><a>发帖 </a></el-col>
+            <el-col :span="8"><i class="el-icon-circle-plus-outline"/><br/><a>发帖</a> </el-col>
+            <el-col :span="8"><i class="el-icon-circle-plus-outline"/><br/><a>发帖 </a></el-col>
+          </el-row>
+        </div>
+        <div class="right-center">
 
-    </div>
-  </div>
-
-</div>
-
-<div class="right">
-  <div class="right-top">
-  <el-row>
-    <el-col :span="8"><i class="el-icon-circle-plus-outline"/><br/><a>发帖 </a></el-col>
-    <el-col :span="8"><i class="el-icon-circle-plus-outline"/><br/><a>发帖</a> </el-col>
-    <el-col :span="8"><i class="el-icon-circle-plus-outline"/><br/><a>发帖 </a></el-col>
-  </el-row>
-  </div>
-  <div class="right-center">
-
-    <div class="biaoti">
-        <a>今日最热</a>
-    </div>
-    <el-row class="right-bot">
-      <el-col class="hot">1、哈哈哈哈哈哈哈哈哈哈</el-col>
-      <el-col class="hot">2、哈哈哈哈哈哈哈哈哈哈</el-col>
-      <el-col class="hot">3、哈哈哈哈哈哈哈哈哈哈</el-col>
-      <el-col class="hot">4、哈哈哈哈哈哈哈哈哈哈</el-col>
-    </el-row>
-  </div>
-</div>
+          <div class="biaoti">
+            <a>今日最热</a>
+          </div>
+          <el-row class="right-bot">
+            <el-col class="hot">1、哈哈哈哈哈哈哈哈哈哈</el-col>
+            <el-col class="hot">2、哈哈哈哈哈哈哈哈哈哈</el-col>
+            <el-col class="hot">3、哈哈哈哈哈哈哈哈哈哈</el-col>
+            <el-col class="hot">4、哈哈哈哈哈哈哈哈哈哈</el-col>
+          </el-row>
+        </div>
+      </div>
 
 
     </div>
@@ -152,6 +150,7 @@ export default {
   name: 'App',
   data() {
     return {
+      isButton:'0',
       content: '      2018总结\n' +
         '      1.工作篇\n' +
         '      2018是一个多窜的年份，为期一年多的努力，顺利的进入心目中的大厂，但是大厂的活计并没有想象中的那么如意，在大厂里始终是个螺丝钉，冒尖的可能性似乎需要年限来堆，期间又始终充满着期盼和失望\n' +
@@ -197,7 +196,12 @@ export default {
       this.show = !this.show
       this.pl = !pl
     },
+    blur_input(){
+      this.isButton='0';
+    },
     fsbutton(){
+      console.log("调用");
+      this.isButton='1';
       this.button=true
     }
   }
@@ -207,79 +211,79 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  font-size: 16px;
-  width: 100%;
-  height: 100%;
-  background-color: #F6F6F6;
-  position: fixed;
-}
-.el-menu-demo{
-  padding-left: 20%;
-}
-.login{
-  padding-right: 20%;
-}
-.login img{
-  height:100%;
-  width: 100%;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    font-size: 16px;
+    width: 100%;
+    height: 100%;
+    background-color: #F6F6F6;
+    position: fixed;
+  }
+  .el-menu-demo{
+    padding-left: 20%;
+  }
+  .login{
+    padding-right: 20%;
+  }
+  .login img{
+    height:100%;
+    width: 100%;
+  }
 
-.con-art {
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
+  .con-art {
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
 
-}
-.centers{
-  padding-left: 20%;
-  padding-right: 20%;
-  width: 100%;
-  height: 100%;
-  margin-top: 10px;
-}
-.left{
-  width: 10em;float: left;
-  height: 100%;
-  background-color: white;
-  padding: 0 5px;
-}
-.center-div{
-  width:50em;
-  float: left;
-  height: auto;
-  background-color: white;
-  margin-left: 10px;
-  padding: 0 10px;
-}
+  }
+  .centers{
+    padding-left: 20%;
+    padding-right: 20%;
+    width: 100%;
+    height: 100%;
+    margin-top: 10px;
+  }
+  .left{
+    width: 10em;float: left;
+    height: 100%;
+    background-color: white;
+    padding: 0 5px;
+  }
+  .center-div{
+    width:50em;
+    float: left;
+    height: auto;
+    background-color: white;
+    margin-left: 10px;
+    padding: 0 10px;
+  }
   .right{
     float: left;
     margin-left: 10px;
     width: 13em;
   }
-.right-top{
-  height: 60px;
-  background-color: white;
-  padding-top: 20px;
-}
-.el-icon-circle-plus-outline{
-margin-bottom: 6px;
-}
-.right-center{
-  padding-top: 10px;
-  margin-top: 5px;
-  height: 100%;
-  text-align: center;
-  line-height:30px;
-  background-color: white;
-}
+  .right-top{
+    height: 60px;
+    background-color: white;
+    padding-top: 20px;
+  }
+  .el-icon-circle-plus-outline{
+    margin-bottom: 6px;
+  }
+  .right-center{
+    padding-top: 10px;
+    margin-top: 5px;
+    height: 100%;
+    text-align: center;
+    line-height:30px;
+    background-color: white;
+  }
   .biaoti{
     border-radius:5px;
     margin: auto;
@@ -289,12 +293,21 @@ margin-bottom: 6px;
   }
   .right-bot .hot{
     white-space: nowrap;
-  text-overflow: ellipsis;
+    text-overflow: ellipsis;
     overflow: hidden;
     border-bottom: #E7E7E7 solid 1px;
   }
-.item{
-  background-color: #FA7A1F;
-  margin-bottom: 5px;
-}
+  .item{
+    background-color: #FA7A1F;
+    margin-bottom: 5px;
+  }
+  .el_input1{
+    transition-property:all;
+    transition-duration:0.5s;
+    transition-timing-function:ease-out;
+    width: 80%;
+  }
+  .el_input{
+    width: 100%;
+  }
 </style>
