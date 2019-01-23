a5dbec75a78974064ff6813cdb0d4d7729b7259c
alwaysacc <alwaysacc@163.com>
2019-01-23 22:19:42 +0800

    1

--- src/page/article/articleDetails.vue
+++ src/page/article/articleDetails.vue
@@ -140,7 +140,7 @@ export default {
         this.relation.operating = 1
         // 关注
         let params = this.relation
-        follew(params).then(res => {
+        cancelFollew(params).then(res => {
           console.log(res)
         })
       }
