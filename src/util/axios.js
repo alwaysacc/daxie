ddb0516be6293da5fdaa01e505ebe273a5e258bd
alwaysacc <alwaysacc@163.com>
2019-02-26 23:12:03 +0800

    已部署到服务器，，www.alwaysacc.club/dist 明天再改改。

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'qs'
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://www.alwaysacc.club:8080'
 export default {
   fetchGet (url, params = {}) {
     return new Promise((resolve, reject) => {
