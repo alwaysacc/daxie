0906a8343226281a9b95ca8e8d7e4a446c5b17fd
alwaysacc <alwaysacc@163.com>
2019-03-20 09:28:20 +0800

    漏洞修复

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'qs'
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = 'https://www.alwaysacc.club'
+axios.defaults.baseURL = 'https://www.alwaysacc.club/api'
 export default {
   fetchGet (url, params = {}) {
     return new Promise((resolve, reject) => {
